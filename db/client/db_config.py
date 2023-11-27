1e14fea232cb8458e7f58cdcbb61137969382baf
jorgeppr951026 <85379121+jorgeppr951026@users.noreply.github.com>
2023-11-27 12:52:18 -0500

    Update db_config.py

--- db/client/db_config.py
+++ db/client/db_config.py
@@ -4,10 +4,10 @@ from typing import Optional
 
 
 # Configuración de MongoDB
-DATABASE_URL = "mongodb://localhost:27017"
-DB_NAME = "local"
-#DATABASE_URL = "mongodb+srv://jorgeppr95106:951026jp@cluster0.jj3npoh.mongodb.net/?retryWrites=true&w=majority"
-#DB_NAME = "Cluster0"
+#DATABASE_URL = "mongodb://localhost:27017"
+#DB_NAME = "local"
+DATABASE_URL = "mongodb+srv://jorgeppr95106:951026jp@cluster0.jj3npoh.mongodb.net/?retryWrites=true&w=majority"
+DB_NAME = "Cluster0"
 
 
 # Configuración de MongoDB
